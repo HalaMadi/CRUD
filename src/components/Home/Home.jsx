c04eee80c3dcd9cf49ed3ff3e587f5210e60efa0
HalaMadi <Halamadi75@gmail.com>
2025-01-15 23:37:10 +0200

    Delete User

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,20 +6,20 @@ const Home = () => {
   const [users, setUsers] = useState([]);
   const getUser = async () => {
     const { data } = await axios.get(`${import.meta.env.VITE_BURL}/users`)
-    setUsers(data.users)
+    setUsers(data.users||[])
   }
   useEffect(() => {
     getUser();
   }, [users])
   const deleteUser=async(id)=>{
     const { data } = await axios.delete(`${import.meta.env.VITE_BURL}/users/${id}`)
-    setUsers(data.users)
+    setUsers(data.users||[])
   }
   return (
     <>
     <div className="row gap-3">
-      {users.map((user, index) =>
-        <div key={user + index} className="card" style={{ width: '16rem' }}>
+      {users.map(user=>
+        <div key={user._id} className="card" style={{ width: '16rem' }}>
           <div className="card-body">
             <h5 className="card-title">{user.userName}</h5>
             <p className="card-text">{user.email}</p>
